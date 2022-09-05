f33a52fdf51bf58cf62e2cfebaddaf7075a06214
GiovanniPicogna <picogna@usm.lmu.de>
2022-09-05 12:20:34 +0200

    correct mispleed variable in userdef for PDR run

--- Test_Problems/HD/Chemistry/PDR/userdef_output.c
+++ Test_Problems/HD/Chemistry/PDR/userdef_output.c
@@ -15,8 +15,8 @@ void ComputeUserVar (const Data *d, Grid *grid)
     int i, j, k;
     double ***cd, ***cdH2, ***cdCO;
     cd = GetUserVar("cd");
-    cdh2 = GetUserVar("cdH2");
-    cdco = GetUserVar("cdCO");
+    cdH2 = GetUserVar("cdH2");
+    cdCO = GetUserVar("cdCO");
 
     DOM_LOOP(k,j,i){
         cd[k][j][i] = irradiation.column_density[0][k][j][i];
